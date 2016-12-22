fe3189785c941ef46ecdbc530f960098e32b548d
Michael <micahel_trucka@gmx.at>
2016-12-22 11:04:25 +0100

    new version

--- pcp.c
+++ pcp.c
@@ -4,11 +4,10 @@
 #include<stdlib.h>
 #include<unistd.h>
 
-pthread_t tid[2];	// Array für Thread id's
 pthread_mutex_t lock;	// Mutex Variable
 int counter = 0;        // Produktanzahl
-int lagerGrosse = 10;
-int itemBuffer[10];
+int lagerGrosse = 10;   // Lagergröße
+int itemBuffer[10];     // Array für Lager
 
 //Prototypen
 void producer(void *ptr);
@@ -16,88 +15,93 @@ void consumer(void *ptr);
 void insertItem(int item);
 void removeItem(int *item); 
 
-void producer(void *ptr)   // Produzieren od. konsummieren je nach Eingabe
+//Producer Unterprogramm
+void producer(void *tid)   
 {
-    while(1)
+    while(1)   
     {
-        int event;
-        scanf("%d",&event);
-        if(event == 1)
+        int event;  //Variable für Eingabe
+        scanf("%d",&event);     //Einlesen des Events
+        if(event == 1)  //Ausführen wenn Event vorhanden
         {
-            //Mutex sperren
-            pthread_mutex_lock(&lock);
-            int item = rand()+1;
+            pthread_mutex_lock(&lock);  //Mutex sperren
+            int item = rand()+1;    //Random Wert (+1 damit nicht 0 vorkommt)
             printf("\n Produktion gestartet\n");
-            insertItem(item);
+            insertItem(item);   //Aufruf von insertItem
             printf("\n Produktion beendet\n");
-            printf("\n produziertes item: #%d\n",item);
+            printf("\n produziertes item: #%d\n",item); //Produziertes Item anzeigen
             printf("-------------------------\n");
 
-            pthread_mutex_unlock(&lock);
+            pthread_mutex_unlock(&lock);    //Mutex freigeben
         }  
     }   
 }
-    
-void consumer(void *ptr)
+
+//Consumer Unterprogramm  
+void consumer(void *tid)
 {
     while(1)
     {
-        sleep(2);
-        //Mutex sperren
-        pthread_mutex_lock(&lock);
+        sleep(2);   //Alle 2 Sekunden cosumer ausführen
+        pthread_mutex_lock(&lock);  //Mutex sperren
         int item;
         printf("\n Konsum gestartet\n");
-        removeItem(&item);
+        removeItem(&item);  //Aufruf von removeItem
         printf("\n Konsum beendet\n");
-        printf("\n konsumiertes item: #%d\n",item);
+        printf("\n konsumiertes item: #%d\n",item); //konsumiertes Item anzeigen
         printf("-------------------------\n"); 
 
-        pthread_mutex_unlock(&lock); 
+        pthread_mutex_unlock(&lock);    //Mutex freigeben
     } 
 }
 
+//insertItem Unterprogramm
 void insertItem(int item){
     if (counter < lagerGrosse){  //Im Lager befindet sich Platz für weiter Items
-        itemBuffer[counter] = item;
-        counter ++;
-        printf("\n Item zum Lager hinzugefügt:  %d items im Lager\n",counter);
+        itemBuffer[counter] = item; //Item  in Lager(Array) platzieren
+        counter ++; //Anzahl der vorhandenen Items inkrementieren
+        printf("\n Item zum Lager hinzugefügt:  %d items im Lager\n",counter); //Azahl der Items im Lager anzeigen
     }else{
         printf("\n Lager ist Voll\n");
     }
 }
 
+//removeItem Unterprogramm
 void removeItem(int *item){
-    if (counter > 0){
-        *item = itemBuffer[counter-1];
-        counter --;
-        printf("\n Item konsumiert:  %d items im Lager\n", counter);
+    if (counter > 0){   //Im Lager ist mindestens ein Item vorhanden
+        *item = itemBuffer[counter-1];  //Item aus Lager(Array) entfernen
+        counter --; //Anzahl der vorhanden Items dekrementieren
+        printf("\n Item konsumiert:  %d items im Lager\n", counter);    //Anzahl der Items im Lager anzeigen
     }else{
         printf("\n Lager ist leer\n");
-        *item = 0;
+        *item = 0;  //Itemwert auf 0 setzen 
     }
 }
 
+//Hauptprogramm
 int main(void)
 {
-    int i[2];
-    pthread_t thread_1;
-    pthread_t thread_2;
-    int err;
+    int id[2];  //Array für thread id's
+    pthread_t thread_1; //thread 1
+    pthread_t thread_2; //thread 2
+    int err;    //err Variable für error handling 
 
     pthread_mutex_init(&lock, NULL);// Mutex initialisieren
     
-    err = pthread_create(&thread_1, NULL, (void *) &producer, (void *) &i[0]);
+    //Producer thread erstellen
+    err = pthread_create(&thread_1, NULL, (void *) &producer, (void *) &id[0]);
     if (err != 0)
     printf("\ncan't create thread :[%s]", strerror(err));
         
-         
-    err = pthread_create(&thread_2, NULL, (void *) &consumer, (void *) &i[1]);
+    //Consumer thread erstellen     
+    err = pthread_create(&thread_2, NULL, (void *) &consumer, (void *) &id[1]);
     if (err != 0)
     printf("\ncan't create thread :[%s]", strerror(err));
 
+    // Wartet auf terminierung der threads
     pthread_join(thread_1, NULL);
     pthread_join(thread_2, NULL);
 
-    pthread_mutex_destroy(&lock);
+    pthread_mutex_destroy(&lock);   //Mutex auflösen
     return 0;
 }
--- pcp_sem.c
+++ pcp_sem.c
@@ -5,12 +5,11 @@
 #include<unistd.h>
 #include<semaphore.h>
 
-pthread_t tid[2];	// Array für Thread id's
-sem_t cEmpty;
-sem_t cFull;
+sem_t cEmpty;   //semaphore für producer
+sem_t cFull;    //semaphore für consumer
 int counter = 0;        // Produktanzahl
-int lagerGrosse = 10;
-int itemBuffer[10];
+int lagerGrosse = 10;   //lagergröße
+int itemBuffer[10]; //Array für Lager
 
 
 //Prototypen
@@ -19,88 +18,93 @@ void consumer(void *ptr);
 void insertItem(int item);
 void removeItem(int *item); 
 
-void producer(void *ptr)   // Produzieren od. konsummieren je nach Eingabe
+//unterprogramm producer
+void producer(void *ptr)   
 {
     while(1)
     {
-        int event;
-        scanf("%d",&event);
-        if(event == 1)
+        int event;  //variable für eingabe
+        scanf("%d",&event); //einlesen des events
+        if(event == 1)  //ausführen wenn event vorhanden
         {
-            //Mutex sperren
-            sem_wait(&cEmpty);
-            int item = rand()+1;
+            sem_wait(&cEmpty);  //semaphore cEmpty sperren wenn nicht 0 (bei eintritt wir cEmpty decrementiert)
+            int item = rand()+1;    //Random Wert (+1 damit nicht 0 vorkommt)
             printf("\n Produktion gestartet\n");
-            insertItem(item);
+            insertItem(item);   //insertItem aufruf
             printf("\n Produktion beendet\n");
-            printf("\n produziertes item: #%d\n",item);
+            printf("\n produziertes item: #%d\n",item); //produziertes item anzeigen
             printf("-------------------------\n");
 
-            sem_post(&cFull);
+            sem_post(&cFull);   //cFull inkrementieren
         }  
     }   
 }
-    
+
+//unterprogramm consumer    
 void consumer(void *ptr)
 {
     while(1)
     {
-        sleep(2);
+        sleep(2);   //alle 2 sekunden consumer ausführen
         printf("\n Konsumer wartet auf Eintritt in kritischen Bereich\n");
-        //Mutex sperren
-        sem_wait(&cFull);
+        sem_wait(&cFull);   ////semaphore cFull sperren wenn nicht 0 (bei eintritt wir cFull decrementiert)
         printf("\n Konsumer ist in kritischen Bereich eingetretten\n");
         int item;
         printf("\n Konsum gestartet\n");
-        removeItem(&item);
+        removeItem(&item);  //removeItem aufruf
         printf("\n Konsum beendet\n");
-        printf("\n konsumiertes item: #%d\n",item);
+        printf("\n konsumiertes item: #%d\n",item); //konsumiertes item anzeigen
         printf("-------------------------\n"); 
 
-        sem_post(&cEmpty);
+        sem_post(&cEmpty);  //cEmpty inkrementieren
     } 
 }
 
+//unterprogramm insertItem
 void insertItem(int item){
     if (counter < lagerGrosse){  //Im Lager befindet sich Platz für weiter Items
-        itemBuffer[counter] = item;
-        counter ++;
-        printf("\n Item zum Lager hinzugefügt:  %d items im Lager\n",counter);
+        itemBuffer[counter] = item; //item im lager(array) platzieren
+        counter ++; //anzahl der vorhanden items inkrementieren
+        printf("\n Item zum Lager hinzugefügt:  %d items im Lager\n",counter);  //anzahl der items im lager anzeigen
     }else{
         printf("\n Lager ist Voll\n");
     }
 }
 
+//unterprogramm removeItem
 void removeItem(int *item){
-    if (counter > 0){
-        *item = itemBuffer[counter-1];
-        counter --;
-        printf("\n Item konsumiert:  %d items im Lager\n", counter);
+    if (counter > 0){   //im lager bedindet sich mindestens ein item
+        *item = itemBuffer[counter-1]; //item aus lager(array) entfernen
+        counter --; //anzahl der vorhanden items dekrementieren
+        printf("\n Item konsumiert:  %d items im Lager\n", counter);    //anzahl der items im lager anzeigen
     }else{
         printf("\n Lager ist leer\n");
-        *item = 0;
+        *item = 0;  //itemwert auf 0 setzen
     }
 }
 
+//hauptprogramm
 int main(void)
 {
-    int i[2];
-    pthread_t thread_1;
-    pthread_t thread_2;
-    int err;
+    int i[2];   //array für thread id's
+    pthread_t thread_1; //thread 1
+    pthread_t thread_2; //thread 2
+    int err;    //err Variable für error handling 
 
-    sem_init(&cFull, 0, 0);
-    sem_init(&cEmpty, 0, 10);
+    sem_init(&cFull, 0, 0); //semaphore cFull initialisieren
+    sem_init(&cEmpty, 0, 10);   //semaphore cEmpty initialisieren
     
+    //producer thread erstellen
     err = pthread_create(&thread_1, NULL, (void *) &producer, (void *) &i[0]);
     if (err != 0)
     printf("\ncan't create thread :[%s]", strerror(err));
         
-         
+    //consumer thread erstellen     
     err = pthread_create(&thread_2, NULL, (void *) &consumer, (void *) &i[1]);
     if (err != 0)
     printf("\ncan't create thread :[%s]", strerror(err));
 
+    //wartet auf terminierung der threads
     pthread_join(thread_1, NULL);
     pthread_join(thread_2, NULL);
 
